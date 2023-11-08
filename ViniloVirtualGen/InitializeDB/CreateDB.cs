98a803ca9c0acb0f3353100d2a65ce5b6247a951
Álvaro (Beesu) Sanz Abad <beesucontact@gmail.com>
2023-11-08 16:40:22 +0100

    Mejoras de los consoles.wirteLines

--- ViniloVirtualGen/InitializeDB/CreateDB.cs
+++ ViniloVirtualGen/InitializeDB/CreateDB.cs
@@ -113,8 +113,14 @@ public static void InitializeData ()
                 string usuario2 = usuariocen.New_ ("Sara", "1313", "sara@gmail.com", new DateTime (1998, 06, 10),
                         ViniloVirtualGen.ApplicationCore.Enumerated.ViniloVirtual.GeneroUsuarioEnum.femenino,
                         ViniloVirtualGen.ApplicationCore.Enumerated.ViniloVirtual.EstadoUsuarioEnum.normal, "perfil2.jpg");
+                string usuario3 = usuariocen.New_ ("Guillermo", "1414", "guille@gmail.com", new DateTime (2000, 06, 14),
+                        ViniloVirtualGen.ApplicationCore.Enumerated.ViniloVirtual.GeneroUsuarioEnum.masculino,
+                        ViniloVirtualGen.ApplicationCore.Enumerated.ViniloVirtual.EstadoUsuarioEnum.normal, "perfil3.jpg");
+                Console.WriteLine (" ");
                 Console.WriteLine ("Usuario " + usuario1 + " creado correctamente");
                 Console.WriteLine ("Usuario " + usuario2 + " creado correctamente");
+                Console.WriteLine ("Usuario " + usuario3 + " creado correctamente");
+                Console.WriteLine (" ");
 
                 //Creacion de artistas
                 int artista1 = artistacen.New_ ("Her's",
@@ -125,8 +131,10 @@ public static void InitializeData ()
                         "Banda virtual inglesa creada en 1998 por Damon Albarn y Jamie Hewlett. La banda está compuesta por cuatro miembros ficticios",
                         "artista2.jpg"
                         );
+                Console.WriteLine (" ");
                 Console.WriteLine ("Artista " + artista1 + " creado correctamente");
                 Console.WriteLine ("Artista " + artista2 + " creado correctamente");
+                Console.WriteLine (" ");
 
                 //Creacion de albumes
                 int album1 = albumcen.New_ ("Invitation to Her's", "Segundo album de la banda Her's",
@@ -139,9 +147,11 @@ public static void InitializeData ()
                 int album3 = albumcen.New_ ("Song of Her's", "Primer album de la banda Her's",
                         ViniloVirtualGen.ApplicationCore.Enumerated.ViniloVirtual.GeneroMusicalEnum.indie, "portada3.jpg", artista1,
                         41.43);
+                Console.WriteLine (" ");
                 Console.WriteLine ("Album " + album1 + " creado correctamente");
                 Console.WriteLine ("Album " + album2 + " creado correctamente");
                 Console.WriteLine ("Album " + album3 + " creado correctamente");
+                Console.WriteLine (" ");
 
                 //Creacion de comunidades
                 int comunidad1 = comunidadcen.New_ ("Locos por el Jazz", "comunidad1.jpg", 15);
@@ -175,21 +185,31 @@ public static void InitializeData ()
 
                 //Probar CUSTOMS
                 //USUARIO
-
                 //Modificamos el estado a "2", equivalente a "baneado temporal"
                 usuariocen.ModificarEstado ("alvaro@gmail.com", 2);
-
                 //Modificamos el nombre del usuario
                 usuariocen.ModificarNombre ("alvaro@gmail.com", "Alvaro");
-
                 //Modificamos la imagen del usuario
                 usuariocen.ModificarImagen ("alvaro@gmail.com", "perfil1.jpg");
 
+                UsuarioEN usuarioen1 = usuariorepository.GetID (usuario1);
+
+                Console.WriteLine (" ");
+                Console.WriteLine ("Estado de " + usuario1 + " cambiado a: " + usuarioen1.Estado);
+                Console.WriteLine ("Nombre de " + usuario1 + " cambiado a: " + usuarioen1.Nombre);
+                Console.WriteLine ("Imagen de perfil de " + usuario1 + " cambiado a: " + usuarioen1.Imagen);
+                Console.WriteLine (" ");
+
 
                 //ALBUM
-
                 //Modificamos el precio del album
                 albumcen.ModificarPrecio (album1, 21.42);
+
+                AlbumEN albumen1 = albumrepository.GetID (album1);
+
+                Console.WriteLine (" ");
+                Console.WriteLine ("Precio del album " + album1 + " cambiado a: " + albumen1.Precio);
+                Console.WriteLine (" ");
 
 
                 //ARTISTA
@@ -200,12 +220,19 @@ public static void InitializeData ()
                 //Modificamos la imagen del artista
                 artistacen.ModificarImagen (artista1, "artista1.jpg");
 
-                /* Esto no funciona, el metodo no cambia correctamente la contraseña. Seguramente por temas de encriptado.
+                /* Esta no funciona, el metodo no cambia correctamente la contraseña. Seguramente por temas de encriptado.
                  * //Modificamos la password e iniciamos sesion
                  * usuariocen.ModificarPass ("alvaro@gmail.com", "1212");
                  * if (usuariocen.Login ("alvaro@gmail.com", "1212") != null) {
                  *      Console.WriteLine ("Usuario loggeado");
                  * }*/
+
+                ArtistaEN artistaen1 = artistarepository.GetID (artista1);
+
+                Console.WriteLine (" ");
+                Console.WriteLine ("Descripcion del artista " + artista1 + " cambiada a: " + artistaen1.Descripcion);
+                Console.WriteLine ("Imagen del artista " + artista1 + " cambiada a: " + artistaen1.Imagen);
+                Console.WriteLine (" ");
 
 
                 //Probar FILTERS
@@ -214,64 +241,76 @@ public static void InitializeData ()
                 IList<UsuarioEN> listaUsuariosEstado =
                         usuariocen.GetUsuariosEstado (ViniloVirtualGen.ApplicationCore.Enumerated.ViniloVirtual.EstadoUsuarioEnum.normal);
 
+                Console.WriteLine (" ");
                 Console.WriteLine ("Consulta de los usuarios con determinado estado ");
-
 
                 foreach (UsuarioEN usuario in listaUsuariosEstado) { // recorrer la lista
                         Console.WriteLine ("El usuario: " + usuario.Email + " . De nombre: " + usuario.Nombre);
                 }
+                Console.WriteLine (" ");
 
                 // FILTRO PARA COMPROBAR LOS ALBUMES DE UN ARTISTA ESPECIFICO
                 IList<AlbumEN> listaAlbumesArtista = albumcen.GetAlbumesArtista (32768);
 
+                Console.WriteLine (" ");
                 Console.WriteLine ("Consulta de los albumes del artista con id 32768 ");
 
                 foreach (AlbumEN album in listaAlbumesArtista) { // recorrer la lista
                         Console.WriteLine ("Album " + album.Nombre + " con ID " + album.Id);
                 }
+                Console.WriteLine (" ");
 
                 // FILTROS DE COMENTARIOS -----------------------------------------------------------------------------
                 // FILTRO PARA COMPROBAR LOS COMENTARIOS DE UN ALBUM ESPECIFICO
                 IList<ComentarioAlbEN> listaCommentsAlbum = comentarioalbcen.GetComentariosAlbum (album2);
 
+                Console.WriteLine (" ");
                 Console.WriteLine ("Consulta de los comentarios del album con id " + album2);
 
                 foreach (ComentarioAlbEN comentario in listaCommentsAlbum) { // recorrer la lista
                         Console.WriteLine ("Comentario: " + comentario.Id + ": " + comentario.Texto);
                 }
+                Console.WriteLine (" ");
 
                 // FILTRO PARA COMPROBAR LOS COMENTARIOS DE UNA COMUNIDAD ESPECIFICO
                 IList<ComentarioComEN> listaCommentsComunidad = comentariocomcen.GetComentariosComunidad (comunidad3);
 
+                Console.WriteLine (" ");
                 Console.WriteLine ("Consulta de los comentarios de la comunidad con id " + comunidad3);
 
                 foreach (ComentarioComEN comentario in listaCommentsComunidad) { // recorrer la lista
-                        Console.WriteLine ("Comentario: " + comentario.Id + ": " + comentario.Texto);
+                        Console.WriteLine ("Comentario " + comentario.Id + ": " + comentario.Texto);
                 }
+                Console.WriteLine (" ");
 
                 // FILTROS DE USUARIOS -----------------------------------------------------------------------------
                 // FILTRO PARA COMPROBAR LOS PEDIDOS DE UN USUARIO EN ESPECIFICO
                 IList<PedidoEN> listaPedidosUsuario = pedidocen.GetPedidosUsu (usuario1);
 
+                Console.WriteLine (" ");
                 Console.WriteLine ("Consulta de los pedidos del usuario con email: " + usuario1);
 
                 foreach (PedidoEN pedido in listaPedidosUsuario) { // recorrer la lista
                         Console.WriteLine ("Pedido " + pedido.Id + " con estado " + pedido.Estado);
                 }
+                Console.WriteLine (" ");
 
                 // FILTROS DE FAVORITOS -----------------------------------------------------------------------------
                 // FILTRO PARA COMPROBAR LOS ALBUMES FAVORITOS DE UN USUARIO EN ESPECIFICO
                 IList<AlbumEN> listaAlbumesFavs = albumcen.GetAlbumesFavsUsu (usuario2);
 
+                Console.WriteLine (" ");
                 Console.WriteLine ("Consulta de los ALBUMES FAVORITOS de email: " + usuario2);
 
                 foreach (AlbumEN album in listaAlbumesFavs) { // recorrer la lista
                         Console.WriteLine ("Album " + album.Nombre);
                 }
+                Console.WriteLine (" ");
 
                 // FILTRO PARA COMPROBAR LOS ARTISTAS FAVORITOS DE UN USUARIO EN ESPECIFICO
                 IList<ArtistaEN> listaArtistasFavs = artistacen.GetArtistasFavsUsu(usuario1);
 
+                Console.WriteLine (" ");
                 Console.WriteLine("Consulta de los ARTISTAS FAVORITOS de email: " + usuario1);
 
                 foreach (ArtistaEN artista in listaArtistasFavs)
