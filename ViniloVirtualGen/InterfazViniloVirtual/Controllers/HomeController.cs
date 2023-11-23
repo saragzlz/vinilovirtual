90e044cfebcdda1bc77b694c02dc92ff3b6bda7a
saragzlz <nori.saragzlez@gmail.com>
2023-11-23 12:48:01 +0100

    update con error
    error en el query de los usuarios con los apellidos al lanzarlo

---
+++ ViniloVirtualGen/InterfazViniloVirtual/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using InterfazViniloVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ViniloVirtualGen.ApplicationCore.CEN.ViniloVirtual;
+using ViniloVirtualGen.ApplicationCore.EN.ViniloVirtual;
+using ViniloVirtualGen.Infraestructure.Repository.ViniloVirtual;
+
+namespace InterfazViniloVirtual.Controllers
+{
+    public class HomeController : BasicController
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            UsuarioRepository usurepo = new UsuarioRepository();
+            UsuarioCEN usucen = new UsuarioCEN(usurepo);
+
+            IEnumerable<UsuarioEN> usuCli = usucen.ReadAll(0, -1);
+
+            return View(usuCli);
+        }   
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
