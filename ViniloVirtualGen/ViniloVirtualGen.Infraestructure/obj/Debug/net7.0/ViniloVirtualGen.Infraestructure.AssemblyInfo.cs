065a2eb91667f57bab9ee6a8a1e5c98bcd3f22eb
adriaangzl <adrian003gonzalez@gmail.com>
2023-12-19 18:12:12 +0100

    login modificado

--- ViniloVirtualGen/ViniloVirtualGen.Infraestructure/obj/Debug/net7.0/ViniloVirtualGen.Infraestructure.AssemblyInfo.cs
+++ ViniloVirtualGen/ViniloVirtualGen.Infraestructure/obj/Debug/net7.0/ViniloVirtualGen.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViniloVirtualGen.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eaa992a1136b8a5eaef37cbc99dcbb138238991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViniloVirtualGen.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViniloVirtualGen.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
