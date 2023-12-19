d5fbc8c2000a72b243a4a500deea677a59bafe87
Álvaro (Beesu) Sanz Abad <beesucontact@gmail.com>
2023-12-19 18:39:50 +0100

    Al pulsar "Like" de album cambian los likes

--- ViniloVirtualGen/InterfazViniloVirtual/Controllers/AlbumController.cs
+++ ViniloVirtualGen/InterfazViniloVirtual/Controllers/AlbumController.cs
@@ -103,6 +103,7 @@ namespace InterfazViniloVirtual.Controllers
                 if (!alb.IsFav)
                 {
                     albCEN.AnyadirFavorito(alb.Id, new List<string>() { user.Email });
+                    albCEN.IncrementoLikes(alb.Id);
                     user.album_favoritos.Add(alb.Id);
                     HttpContext.Session.Set<UsuarioViewModel>("usuario", user);
 
@@ -110,6 +111,7 @@ namespace InterfazViniloVirtual.Controllers
                 else
                 {
                     albCEN.EliminarFavorito(alb.Id, new List<string>() { user.Email });
+                    albCEN.DecrementoLikes(alb.Id);
                     user.album_favoritos.Remove(alb.Id);
                     HttpContext.Session.Set<UsuarioViewModel>("usuario", user);
                 }
